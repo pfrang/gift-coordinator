9db05fd585e4b41d47e76d4c2e3d6234b345316c
peder frang <peder.frang@gmail.com>
2022-05-14 16:39:53 +0200

    added_backend

--- pages/components/Forms/FrontPageForm.tsx
+++ pages/components/Forms/FrontPageForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import mongoDB from '../../../sql-nodejs/cosmosdb/app'
 import { useRouter } from 'next/router';
 
 
@@ -8,16 +9,26 @@ import { useRouter } from 'next/router';
 function FrontPageForm(props) {
 
   const router = useRouter()
+  const db = new mongoDB
 
-  const onSubmitJoin = (e) => {
+
+  const onSubmitJoin = async (e) => {
     e.preventDefault()
     const val = e.target.children[0].value
-    router.push(`/lobby/${val}`)
+    console.log(val)
+    console.log(typeof parseInt(val, 10))
+    const response = await db.read(`SELECT * from c where c.id = '${val}'`);
+    const id = response.resources[0].id
+    router.push(`/lobby/${id}`)
   }
-  const onSubmitCreate = (e) => {
+  const onSubmitCreate = async (e) => {
     e.preventDefault()
-    const val = e.target.children[0].value
-    router.push(`/lobby/${val}`)
+    const id = e.target.children[0].value
+    const lobby = {
+      id: id
+    }
+    const response = await db.insert(lobby);
+    router.push(`/lobby/${id}`)
   }
 
 
