2af80966f9e6fb6335f22f263b1a459dcfd501e8
peder frang <peder.frang@gmail.com>
2022-06-21 13:01:20 +0200

    security-headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,39 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: "SAMEORIGIN",
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: "max-age=31536000; includeSubDomains"
+          },
+          {
+            key: "Referrer-policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self'; style-src 'self' 'unsafe-inline'",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "microphone=()"
+          }
+        ]
+      }
+    ]
+  }
+}
