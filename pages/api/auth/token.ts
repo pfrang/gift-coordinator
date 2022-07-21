7473ddeb2670526789e1208f713f5427deaa25e5
peder frang <peder.frang@gmail.com>
2022-07-22 01:51:16 +0200

    push

--- pages/api/auth/token.ts
+++ pages/api/auth/token.ts
@@ -1,10 +1,10 @@
-// import { faJarWheat } from "@fortawesome/free-solid-svg-icons";
-// import { NextApiRequest, NextApiResponse } from "next";
-// import * as jwt from "next-auth/jwt";
+import { faJarWheat } from "@fortawesome/free-solid-svg-icons";
+import { NextApiRequest, NextApiResponse } from "next";
+import * as jwt from "next-auth/jwt";
 
-// const secret = process.env.NEXTAUTH_SECRET
+const secret = process.env.NEXTAUTH_SECRET
 
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   const token = await jwt.getToken({req, secret});
-//   res.send(JSON.stringify(token, undefined, 2));
-// }
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const token = await jwt.getToken({req, secret});
+  res.send(JSON.stringify(token, undefined, 2));
+}
