64166afa676522579d7b285c81bf8072be3c4316
peder frang <peder.frang@gmail.com>
2023-11-21 18:53:05 +0100

    styling-fixes?

--- src/pages/components/styling-divs/layout.tsx
+++ src/pages/components/styling-divs/layout.tsx
@@ -6,7 +6,7 @@ import Header from "../header";
 
 const Wrapper = styled.div`
   min-height: 100vh;
-  min-width: 100vh;
+
   display: grid;
   grid-template-rows: auto minmax(0, 1fr) auto;
   background-color: #0d1e45ef;
